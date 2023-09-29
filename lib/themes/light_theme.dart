72daba27096d3b8f803a2f560740f19a58872c38
edilayehu <tadesseedilayehu@gmail.com>
2023-09-29 13:20:06 +0300

    Implemented Theme
    Defined text and button themes for a clean and consistent user interface.

---
+++ lib/themes/light_theme.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+final lightTheme = ThemeData(
+  primarySwatch: Colors.blue,
+  hintColor: Colors.orange,
+  brightness: Brightness.light,
+
+  // Define the text theme
+  textTheme: const TextTheme(
+    displayLarge: TextStyle(
+      fontSize: 28.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+    displayMedium: TextStyle(
+      fontSize: 24.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+    displaySmall: TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+    bodyLarge: TextStyle(
+      fontSize: 16.0,
+      color: Colors.black,
+    ),
+    bodyMedium: TextStyle(
+      fontSize: 14.0,
+      color: Colors.black,
+    ),
+  ),
+
+  // Define the button theme
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.black,
+      textStyle: const TextStyle(
+        fontSize: 16.0,
+        fontWeight: FontWeight.bold,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+    ),
+  ),
+);
